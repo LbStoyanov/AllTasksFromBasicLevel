67e0a43de2ecabab00ec850f1d6aa1a06700f7ea
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-30 16:45:24 +0300

    Problem 02.Bracelet Stand solved

---
+++ 02. Bracelet Stand/02. Bracelet Stand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02._Bracelet_Stand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pocketMoneyPerDay = double.Parse(Console.ReadLine());
+            double moneyEarnedFromSells = double.Parse(Console.ReadLine());
+            double expensesForWholePeriod = double.Parse(Console.ReadLine());
+            double presentPrice = double.Parse(Console.ReadLine());
+
+            double savedPocketMoney = pocketMoneyPerDay * 5;
+            double savedMoneyfromSels = moneyEarnedFromSells * 5;
+
+            double totalMoneysaved = savedMoneyfromSels + savedPocketMoney;
+            double finalAmountleft = totalMoneysaved - expensesForWholePeriod;
+
+            
+            
+
+            if (finalAmountleft > presentPrice)
+            {
+                //var moneyNeeded = Math.Abs(moneyLeft - presentPrice);
+                Console.WriteLine($"Profit: {finalAmountleft:f2} BGN, the gift has been purchased.");
+            }
+            else
+            {
+                var difference = presentPrice - finalAmountleft;
+                Console.WriteLine($"Insufficient money: {difference:f2} BGN.");
+            }
+
+
+        }
+    }
+}
